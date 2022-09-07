38ae071469302c141b676ec0f424fe75bc11a36a
Otto Jolanki <ojolanki@stanford.edu>
2022-09-07 10:48:58 -0700

    add datadog secret

--- src/shared_infrastructure/igvf_dev/secret.py
+++ src/shared_infrastructure/igvf_dev/secret.py
@@ -14,3 +14,13 @@ class DockerHubCredentials(Construct):
             'DockerSecret',
             'arn:aws:secretsmanager:us-west-2:109189702753:secret:docker-hub-credentials-EStRH5',
         )
+
+class DataDogAPIKey(Construct):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.secret = Secret.from_secret_complete_arn(
+            self,
+            'DataDogSecret',
+            'arn:aws:secretsmanager:us-west-2:109189702753:secret:datadog-api-key-L8sXwf',
+        )
