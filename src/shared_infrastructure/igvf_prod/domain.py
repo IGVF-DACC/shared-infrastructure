6fe9897aa368e270f92b20513ae3e2e6f9ce6145
Keenan Graham <ksgraham@stanford.edu>
2022-11-29 17:48:51 -0800

    Update prod domain

--- src/shared_infrastructure/igvf_prod/domain.py
+++ src/shared_infrastructure/igvf_prod/domain.py
@@ -10,11 +10,11 @@ class Domain(Construct):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        self.name = 'staging.igvf.org'
+        self.name = 'data.igvf.org'
         self.certificate = Certificate.from_certificate_arn(
             self,
             'DomainCertificate',
-            'arn:aws:acm:us-west-2:920073238245:certificate/e0f25309-7ce4-4b20-a1c9-df951b18e1c7',
+            'arn:aws:acm:us-west-2:035226225042:certificate/c633a0fd-5bd0-4869-86c4-66c48cadf785',
         )
         self.zone = HostedZone.from_lookup(
             self,
