4dbd81f39dc84246b351dc7d466c862f1459c1f9
Otto Jolanki <ojolanki@stanford.edu>
2023-11-01 11:29:34 -0700

    add bus connection domain environment secret

---
+++ src/shared_infrastructure/igvf_catalog_dev/domain.py
@@ -0,0 +1,23 @@
+from constructs import Construct
+
+from aws_cdk.aws_certificatemanager import Certificate
+from aws_cdk.aws_route53 import HostedZone
+
+from typing import Any
+
+
+class DemoDomain(Construct):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        self.name = 'catalog.igvf.org'
+        self.certificate = Certificate.from_certificate_arn(
+            self,
+            'DomainCertificate',
+            'arn:aws:acm:us-west-2:109189702753:certificate/84e0ade9-fb95-49e9-bf38-fb663cc38d55'
+        )
+        self.zone = HostedZone.from_lookup(
+            self,
+            'DomainZone',
+            domain_name=self.name,
+        )
